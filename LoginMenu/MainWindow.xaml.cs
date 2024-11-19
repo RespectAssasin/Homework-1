f04d2c87c8f2809248f9e46affe2330cace3cb22
RespectAssasin <makarovs220@gmail.com>
2024-11-19 23:34:15 +0300

    19.11

---
+++ LoginMenu/MainWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LoginMenu
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private List<string> _userNames = new List<string>();
+        private List<string> _emails = new List<string>();
+        private List<string> _passwords = new List<string>();
+
+        public List<string> UserNames { get { return _userNames; } }
+        public List<string> Emails { get { return _emails; } }
+        public List<string> Passwords { get { return _passwords; } }
+
+        private void GoToLogin(object sender, RoutedEventArgs e)
+        {
+            MainMenuPanel.Visibility = Visibility.Collapsed;
+            RegistrationPanel.Visibility = Visibility.Collapsed;
+            UserPanel.Visibility = Visibility.Collapsed;
+            LoginPanel.Visibility = Visibility.Visible;
+        }
+        private void GoToRegistration(object sender, RoutedEventArgs e)
+        {
+            MainMenuPanel.Visibility = Visibility.Collapsed;
+            RegistrationPanel.Visibility = Visibility.Visible;
+            UserPanel.Visibility = Visibility.Collapsed;
+            LoginPanel.Visibility = Visibility.Collapsed;
+        }
+        private void GoToMenu(object sender, RoutedEventArgs e)
+        {
+            UserPanel.Visibility = Visibility.Collapsed;
+            MainMenuPanel.Visibility = Visibility.Visible;
+        }
+
+        private void Login(object sender, RoutedEventArgs e)
+        {
+            if (UsernameBox.Text == string.Empty || PasswordBox.Password == string.Empty) 
+            {
+                MessageBox.Show("Ошибка, вы не заполнили все поля");
+                return;
+            }
+            User user = new User(UsernameBox.Text, PasswordBox.Password);
+
+            if (!user.IsExist(UserNames, Emails, Passwords))
+            {
+                MessageBox.Show($"Ошибка, пользователь не найден\nВозможно неправильный логин или пароль{user.UserName}, {user.Email}, {user.Password}, {user.IsExist(UserNames, Emails, Passwords)}");
+                return;
+            } else
+            {
+                LoginPanel.Visibility = Visibility.Collapsed;
+                UserPanel.Visibility = Visibility.Visible;
+
+                UsernameInfo.Text = user.UserName;
+                EmailInfo.Text = user.Email;
+                PasswordInfo.Text = user.Password;
+            }
+        }
+
+        private void Registration(object sender, RoutedEventArgs e)
+        {
+            if (UserNameBox.Text == string.Empty || EmailBox.Text == String.Empty || FirstPasswordBox.Password == string.Empty || SecondPasswordBox.Password == string.Empty) 
+            {
+                MessageBox.Show("Ошибка, неправильный логин или пароль");
+                return;
+            }
+            if (FirstPasswordBox.Password != SecondPasswordBox.Password)
+            {
+                MessageBox.Show("Ошибка, Первый пароли не одинаковы");
+            }
+
+            User user = new User(UserNameBox.Text, EmailBox.Text, FirstPasswordBox.Password);
+            user.AddUser(_userNames, _emails, _passwords);
+            if (!user.IsExist(UserNames, Emails, Passwords))
+            {
+                MessageBox.Show("Пользователь не добавлен\nps(не надо ломать меня)");
+            } else
+            {
+                MessageBox.Show($"Поздравляем с регистрацией.\nПерезайдите в приложение, чтобы войти в аккаунт\n{user.IsExist(UserNames, Emails, Passwords)}, ");
+                
+
+                RegistrationPanel.Visibility = Visibility.Collapsed;
+                MainMenuPanel.Visibility = Visibility.Visible;
+
+                UserNameBox.Text = string.Empty;
+                EmailBox.Text = string.Empty;
+                FirstPasswordBox.Password = string.Empty;
+                SecondPasswordBox.Password = string.Empty;
+            }
+            
+        }
+
+        
+    }
+}
---
+++ LoginMenu/User.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Packaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginMenu
+{
+    internal class User
+    {
+        
+        private string _userName;
+        private string _email;
+        private string _password;
+        private string _nameoremail;
+        private int id1;
+        private int id2;
+
+        public string UserName { get { return _userName; } }
+        public string Email { get { return _email; } }
+        public string Password { get { return _password; } }
+        public string NameOrEmail {  get { return _nameoremail; } }
+        public void SetEmail(string email)
+        {
+            this._email = email;
+        }
+        public void SetUsername(string username)
+        {
+            this._userName = username;
+        }
+        public User() { }
+        public User(string username, string email, string password) 
+        {
+            _userName = username;
+            _email = email;
+            _password = password;
+        }
+        public User(string nameOrEmail, string password)
+        {
+            _nameoremail = nameOrEmail;
+            _password = password;
+        }
+        public bool IsExist(List<string> usernames, List<string> emails, List<string> passwords)
+        {
+            if (this.UserName != string.Empty && usernames.Contains(this.UserName))
+            {
+                id1 = usernames.IndexOf(this.UserName);
+                id2 = passwords.IndexOf(this.Password);
+                
+                if (id1 == id2)
+                {
+                    this.SetEmail(emails[id1]);
+                    return true;
+                } 
+            }
+            else if (this.Email != string.Empty && emails.Contains(this.Email))
+            {
+                id1 = emails.IndexOf(this.Email);
+                id2 = passwords.IndexOf(this.Password);
+
+                if (id1 == id2)
+                {
+                    this.SetUsername(usernames[id1]);
+                    return true;
+                }
+            }
+            return false;
+        }
+        public void AddUser(List<string> usernames, List<string> emails, List<string>  passwords)
+        {
+            usernames.Add(this.UserName);
+            emails.Add(this.Email);
+            passwords.Add(this.Password);
+        }
+    }
+}
