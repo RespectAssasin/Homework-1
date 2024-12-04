80beb3738060a370c372c86f2db4ec15fe294f0e
RespectAssasin <128396252+RespectAssasin@users.noreply.github.com>
2024-12-04 20:50:57 +0300

    +

--- Minesweeper/MainWindow.xaml.cs
+++ Minesweeper/MainWindow.xaml.cs
@@ -20,9 +20,40 @@ namespace Minesweeper
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int _gameFieldHight = 10;
+        private int _gameFieldWidth = 10;
+        private float _minesPercent = 0.15f;
+        private bool [,] _gameField;
+        private int _minesNumber;
         public MainWindow()
         {
             InitializeComponent();
         }
+        
+        private void GenerateGameField()
+        {
+            _gameField = new bool[_gameFieldWidth, _gameFieldHight];
+            _minesNumber = (int)((float)(_gameFieldHight * _gameFieldWidth) * _minesPercent);
+        
+            for (int i = 0; i < _minesNumber; i++ )
+            {
+                int row = Next(_gameFieldWidth);
+            }
+        }
+
+        private void GenerateUIField()
+        {
+            Grid gameField = new Grid();
+            GamePanel.Children.Add(gameField);
+            gameField.Margin = new Thickness(80, 130, 80, 10);
+
+            for (int i = 0; i < _gameFieldHight; i++)
+            {
+                RowDefinition newRow = new RowDefinition();
+                gameField.RowDefinitions.Add(newRow);
+            }
+
+        }
+
     }
 }
