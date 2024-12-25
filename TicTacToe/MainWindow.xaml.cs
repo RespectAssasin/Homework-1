3565f5d14fd099e75957643b8f254387c0df0428
RespectAssasin <128396252+RespectAssasin@users.noreply.github.com>
2024-12-25 16:57:30 +0300

    +game

--- TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/MainWindow.xaml.cs
@@ -1,164 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TicTacToe
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private TicTacToeLogic _logic;
+        private string _gameType;
+        private int _difficult;
+        private char _userChar;
+
         public MainWindow()
         {
             InitializeComponent();
-        }
-        double Difficulty = 0;
-
-        TicTacToeLogic logic;
-
-        private string _gameType = "";
-        private int _difficult = 0;
-        private char _userChar = '\0';
-
-
-        //private char[][] arr = new char[3][];
-
-        private void GameClick(object sender, RoutedEventArgs e)
-        {
-            var button = sender as Button;
-            if (button != null)
-            {
-                string buttonName = button.Name;
-                if (buttonName.Length >= 2)
-                {
-                    int x, y;
-                    if (int.TryParse(buttonName[buttonName.Length - 2].ToString(), out x) &&
-                        int.TryParse(buttonName[buttonName.Length - 1].ToString(), out y))
-                    {
-                        if (x >= 0 && x < 3 && y >= 0 && y < 3)
-                        {
-                            //arr[3][3] = (char)button.Content;
-
-                            //MessageBox.Show($"Значение '{arr[3][3]}' добавлено в массив на место ({x}, {y})");
-                        }
-                        else
-                        {
-                            MessageBox.Show("Координаты за пределами допустимого диапазона.");
-                        }
-                    }
-                    else
-                    {
-                        MessageBox.Show("Не удалось извлечь координаты из имени кнопки.");
-                    }
-                }
-                else
-                {
-                    MessageBox.Show("Имя кнопки слишком короткое.");
-                }
-            }
-
+            this._gameType = "";
+            this._difficult = 1;
+            this._userChar = '\0';
         }
 
-        private void BackToMenu(object sender, RoutedEventArgs e)
+        private async void StartGame(object sender, RoutedEventArgs e)
         {
-            GamePanel.Visibility = Visibility.Collapsed;
-            PvPSettingsPanel.Visibility = Visibility.Collapsed;
-            PvESettingsPanel.Visibility = Visibility.Collapsed;
-            MainMenuPanel.Visibility = Visibility.Visible;
-            _gameType = "";
-            _difficult = 0;
-            _userChar = ' ';
-        }
-
-        private void GoToPvPSettings(object sender, RoutedEventArgs e)
-        {
-            MainMenuPanel.Visibility = Visibility.Collapsed;
-            PvPSettingsPanel.Visibility = Visibility.Visible;
-            _gameType = "PvP";
-        }
-        private void GoToPvESettings(object sender, RoutedEventArgs e)
-        {
-            MainMenuPanel.Visibility = Visibility.Collapsed;
-            PvESettingsPanel.Visibility = Visibility.Visible;
-            _gameType = "PvE";
-        }
-        private void GoToEvESettings(object sender, RoutedEventArgs e)
-        {
-            MainMenuPanel.Visibility = Visibility.Collapsed;
-            PvESettingsPanel.Visibility = Visibility.Visible;
-            _gameType = "EvE";
-        }
-
-        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-
-            Difficulty = DifficultySlider.Value;
-
-            /*if (DifficultySlider.Value == 1 && DifficultySlider.Value > 1)
-            {
-                DifficultySlider.Value = 2;
-            } else if (DifficultySlider.Value == 2 && DifficultySlider.Value > 2)
-            {
-                DifficultySlider.Value = 3;
-            } else if (DifficultySlider.Value == 3 && DifficultySlider.Value < 3)
-            {
-                DifficultySlider.Value = 2;
-            } else if (DifficultySlider.Value == 2 && DifficultySlider.Value < 2)
-            {
-                DifficultySlider.Value = 1;
-            }*/
-
-            switch ((int)Difficulty)
-            {
-                case 1:
-                    DifficultSliderBlock.Text = "Лёгкая";
-                    break;
-                case 2:
-                    DifficultSliderBlock.Text = "Нормальная";
-                    break;
-                case 3:
-                    DifficultSliderBlock.Text = "Сложная";
-                    break;
-            }
-        }
-
-        private void StartGame(object sender, RoutedEventArgs e)
-        {
-
-            if (_gameType == "" || _userChar == '\0')
+            if (this._gameType == "" || this._userChar == '\0')
             {
                 MessageBox.Show("Вы не заполнили все поля");
                 return;
             }
-            if (!(_gameType == "PvP") && _difficult == 0)
+            if (this._gameType != "PvP" && this._difficult == 0)
             {
                 MessageBox.Show("Вы не заполнили сложность");
                 return;
             }
-
-            PvPSettingsPanel.Visibility = Visibility.Collapsed;
-            PvESettingsPanel.Visibility = Visibility.Collapsed;
-            GamePanel.Visibility = Visibility.Visible;
+            this.PvESettingsPanel.Visibility = Visibility.Collapsed;
+            this.GamePanel.Visibility = Visibility.Visible;
 
             if ((bool)RadioButt_X.IsChecked)
             {
-                _userChar = 'X';
+                this._userChar = 'X';
             }
             else if ((bool)RadioButt_O.IsChecked)
             {
-                _userChar = 'O';
+                this._userChar = 'O';
             }
             else
             {
@@ -166,8 +50,327 @@ namespace TicTacToe
                 return;
             }
 
-            logic = new TicTacToeLogic();
+            this._logic = new TicTacToeLogic();
+            this.TurnBox.Text = this._logic.CurrentPlayer;
 
+            if (this._gameType == "PvE")
+            {
+                if (this._userChar == 'O' && this._logic.CurrentPlayer == "X")
+                {
+                    await Task.Delay(500);
+                    this._logic.MakeAiMove(this._difficult, "X");
+                    UpdateBoardUI();
+                    if (this._logic.GameOver)
+                    {
+                        EndGame();
+                    }
+                    else
+                    {
+                        this.TurnBox.Text = this._logic.CurrentPlayer;
+                    }
+                }
+            }
+        }
+
+        private Image GetSymbolImage(string symbol)
+        {
+            if (string.IsNullOrEmpty(symbol)) return null;
+            string imageFile = symbol == "X" ? "X.png" : "O.png";
+            Image img = new Image();
+            Uri uri = new Uri("pack://application:,,,/" + imageFile, UriKind.Absolute);
+            BitmapImage bmp = new BitmapImage(uri);
+            img.Source = bmp;
+            img.Stretch = System.Windows.Media.Stretch.Uniform;
+            img.VerticalAlignment = System.Windows.VerticalAlignment.Center;
+            img.HorizontalAlignment = System.Windows.HorizontalAlignment.Center;
+            return img;
+        }
+
+        private void UpdateBoardUI()
+        {
+            Butt00.Content = GetSymbolImage(_logic.Board[0]);
+            Butt01.Content = GetSymbolImage(_logic.Board[1]);
+            Butt02.Content = GetSymbolImage(_logic.Board[2]);
+            Butt10.Content = GetSymbolImage(_logic.Board[3]);
+            Butt11.Content = GetSymbolImage(_logic.Board[4]);
+            Butt12.Content = GetSymbolImage(_logic.Board[5]);
+            Butt20.Content = GetSymbolImage(_logic.Board[6]);
+            Butt21.Content = GetSymbolImage(_logic.Board[7]);
+            Butt22.Content = GetSymbolImage(_logic.Board[8]);
+        }
+
+        private void EndGame()
+        {
+            Butt00.IsEnabled = false;
+            Butt01.IsEnabled = false;
+            Butt02.IsEnabled = false;
+            Butt10.IsEnabled = false;
+            Butt11.IsEnabled = false;
+            Butt12.IsEnabled = false;
+            Butt20.IsEnabled = false;
+            Butt21.IsEnabled = false;
+            Butt22.IsEnabled = false;
+
+            PlayAgainButton.IsEnabled = true;
+
+            if (_logic.Winner == "Draw")
+            {
+                MessageBox.Show("Ничья!");
+            }
+            else
+            {
+                MessageBox.Show("Победил " + _logic.Winner);
+            }
+        }
+        private async void GameClick(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            if (button != null && button.Name.Length >= 2)
+            {
+                int x, y;
+                if (int.TryParse(button.Name[button.Name.Length - 2].ToString(), out x) &&
+                    int.TryParse(button.Name[button.Name.Length - 1].ToString(), out y))
+                {
+                    if (this._logic != null && !this._logic.GameOver)
+                    {
+                        if (this._gameType == "PvE" && this._logic.CurrentPlayer != this._userChar.ToString())
+                            return;
+
+                        this._logic.MakeMove(x, y);
+                        UpdateBoardUI();
+                        this.TurnBox.Text = this._logic.CurrentPlayer;
+
+                        if (this._logic.GameOver)
+                        {
+                            EndGame();
+                            return;
+                        }
+                        if (this._gameType == "PvE")
+                        {
+                            await Task.Delay(500);
+                            if (!this._logic.GameOver)
+                            {
+                                this._logic.MakeAiMove(this._difficult, this._logic.CurrentPlayer);
+                                UpdateBoardUI();
+                                if (this._logic.GameOver)
+                                {
+                                    EndGame();
+                                }
+                                else
+                                {
+                                    this.TurnBox.Text = this._logic.CurrentPlayer;
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Не удалось извлечь координаты из имени кнопки.");
+                }
+            }
+        }
+
+        private void BackToMenu(object sender, RoutedEventArgs e)
+        {
+            if (this._logic != null)
+            {
+                this._logic.ResetGame();
+                Butt00.Content = "";
+                Butt01.Content = "";
+                Butt02.Content = "";
+                Butt10.Content = "";
+                Butt11.Content = "";
+                Butt12.Content = "";
+                Butt20.Content = "";
+                Butt21.Content = "";
+                Butt22.Content = "";
+                this.TurnBox.Text = "";
+            }
+            Butt00.IsEnabled = true;
+            Butt01.IsEnabled = true;
+            Butt02.IsEnabled = true;
+            Butt10.IsEnabled = true;
+            Butt11.IsEnabled = true;
+            Butt12.IsEnabled = true;
+            Butt20.IsEnabled = true;
+            Butt21.IsEnabled = true;
+            Butt22.IsEnabled = true;
+
+            this.GamePanel.Visibility = Visibility.Collapsed;
+            this.PvESettingsPanel.Visibility = Visibility.Collapsed;
+            this.MainMenuPanel.Visibility = Visibility.Visible;
+
+            this._gameType = "";
+            this._difficult = 0;
+            this._userChar = '\0';
+        }
+
+        private void GoToPvPSettings(object sender, RoutedEventArgs e)
+        {
+            this._gameType = "PvP";
+            this._userChar = 'X';
+            this.MainMenuPanel.Visibility = Visibility.Collapsed;
+            this.PvESettingsPanel.Visibility = Visibility.Collapsed;
+            this.GamePanel.Visibility = Visibility.Visible;
+
+            this._logic = new TicTacToeLogic();
+            this.TurnBox.Text = this._logic.CurrentPlayer;
+            UpdateBoardUI();
+        }
+
+        private void GoToPvESettings(object sender, RoutedEventArgs e)
+        {
+            this._gameType = "PvE";
+            this.MainMenuPanel.Visibility = Visibility.Collapsed;
+            this.GamePanel.Visibility = Visibility.Collapsed;
+            this.PvESettingsPanel.Visibility = Visibility.Visible;
+        }
+
+        private async void GoToEvESettings(object sender, RoutedEventArgs e)
+        {
+            this._gameType = "EvE";
+            this.MainMenuPanel.Visibility = Visibility.Collapsed;
+            this.PvESettingsPanel.Visibility = Visibility.Collapsed;
+            this.GamePanel.Visibility = Visibility.Visible;
+
+            this._logic = new TicTacToeLogic();
+            this.TurnBox.Text = this._logic.CurrentPlayer;
+            UpdateBoardUI();
+            await StartEvEGame();
+        }
+
+        private async void GoToGamePanel(object sender, RoutedEventArgs e)
+        {
+            if (this._gameType == "PvE")
+            {
+                if (this.RadioButt_X.IsChecked == true)
+                {
+                    this._userChar = 'X';
+                }
+                else if (this.RadioButt_O.IsChecked == true)
+                {
+                    this._userChar = 'O';
+                }
+                else
+                {
+                    MessageBox.Show("Вы не выбрали, за кого вы ходите.");
+                    return;
+                }
+            }
+            PvESettingsPanel.Visibility = Visibility.Collapsed;
+            MainMenuPanel.Visibility = Visibility.Collapsed;
+            GamePanel.Visibility = Visibility.Visible;
+
+            _logic = new TicTacToeLogic();
+            TurnBox.Text = _logic.CurrentPlayer;
+
+            PlayAgainButton.IsEnabled = false;
+
+            if (_gameType == "PvE")
+            {
+                if (_userChar == 'O' && _logic.CurrentPlayer == "X")
+                {
+                    await Task.Delay(500);
+                    _logic.MakeAiMove(_difficult, "X");
+                    UpdateBoardUI();
+                    if (_logic.GameOver)
+                    {
+                        EndGame();
+                    }
+                    else
+                    {
+                        TurnBox.Text = _logic.CurrentPlayer;
+                    }
+                }
+            }
+            if (_gameType == "EvE")
+            {
+                await StartEvEGame();
+            }
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            this._difficult = (int)e.NewValue;
+            switch (this._difficult)
+            {
+                case 1:
+                    this.DifficultSliderBlock.Text = "Лёгкая";
+                    break;
+                case 2:
+                    this.DifficultSliderBlock.Text = "Нормальная";
+                    break;
+                case 3:
+                    this.DifficultSliderBlock.Text = "Сложная";
+                    break;
+            }
+        }
+        private async void PlayAgain(object sender, RoutedEventArgs e)
+        {
+            if (this._logic != null)
+            {
+                this._logic.ResetGame();
+                Butt00.Content = "";
+                Butt01.Content = "";
+                Butt02.Content = "";
+                Butt10.Content = "";
+                Butt11.Content = "";
+                Butt12.Content = "";
+                Butt20.Content = "";
+                Butt21.Content = "";
+                Butt22.Content = "";
+                Butt00.IsEnabled = true;
+                Butt01.IsEnabled = true;
+                Butt02.IsEnabled = true;
+                Butt10.IsEnabled = true;
+                Butt11.IsEnabled = true;
+                Butt12.IsEnabled = true;
+                Butt20.IsEnabled = true;
+                Butt21.IsEnabled = true;
+                Butt22.IsEnabled = true;
+                this.TurnBox.Text = this._logic.CurrentPlayer;
+                UpdateBoardUI();
+                
+            }
+            if (this._gameType == "EvE")
+            {
+                _ = StartEvEGame();
+            }
+            if (_gameType == "PvE" && _userChar == 'O')
+            {
+                await Task.Delay(500);
+                this._logic.MakeAiMove(this._difficult, "X");
+                UpdateBoardUI();
+                if (this._logic.GameOver)
+                {
+                    EndGame();
+                }
+                else
+                {
+                    this.TurnBox.Text = this._logic.CurrentPlayer;
+                }
+            }
+        }
+
+        private async Task StartEvEGame()
+        {
+            if (this._logic == null) return;
+            while (!this._logic.GameOver)
+            {
+                await Task.Delay(500);
+                this._logic.MakeAiMove(3, this._logic.CurrentPlayer);
+                UpdateBoardUI();
+                if (this._logic.GameOver)
+                {
+                    EndGame();
+                    break;
+                }
+                else
+                {
+                    this.TurnBox.Text = this._logic.CurrentPlayer;
+                }
+            }
         }
     }
 }
--- TicTacToe/TicTacToeLogic.cs
+++ TicTacToe/TicTacToeLogic.cs
@@ -1,30 +1,190 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TicTacToe
 {
-    internal class TicTacToeLogic
+    public class TicTacToeLogic
     {
-        private char[][] arr = new char[3][];
+        private string[] _board;
+        private string _currentPlayer;
+        private bool _gameOver;
+        private string _winner;
 
-        public TicTacToeLogic() { }
+        public string[] Board { get { return _board; } }
+        public string CurrentPlayer { get { return _currentPlayer; } }
+        public bool GameOver { get { return _gameOver; } }
+        public string Winner { get { return _winner; } }
 
-        public void AddToArray(int x, int y, char sym)
+        private Random _rnd;
+        public TicTacToeLogic()
         {
-            arr[x][y] = sym;
-        }
-        public void ResetArray()
-        {
-            for (int i = 0; i < arr.Length; i++)
+            this._board = new string[9];
+            for (int i = 0; i < 9; i++)
             {
-                for (int j = 0; j < arr[i].Length; j++)
+                this._board[i] = "";
+            }
+            this._currentPlayer = "X";
+            this._gameOver = false;
+            this._winner = "";
+            this._rnd = new Random();
+        }
+
+        public void MakeMove(int x, int y)
+        {
+            int index = x * 3 + y;
+            if (!this._gameOver && index >= 0 && index < 9 && this._board[index] == "")
+            {
+                this._board[index] = this._currentPlayer;
+                CheckWinCondition();
+                if (!this._gameOver)
                 {
-                    arr[i][j] = '\0';
+                    this._currentPlayer = this._currentPlayer == "X" ? "O" : "X";
                 }
             }
+        }
+
+        public void MakeAiMove(int difficulty, string aiSymbol)
+        {
+            if (_gameOver) return;
+            if (_currentPlayer != aiSymbol) return;
+            if (difficulty == 1)
+            {
+                AiRandom();
+            }
+            else if (difficulty == 2)
+            {
+                AiRandom();
+            }
+            else
+            {
+                AiBestMove(aiSymbol);
+            }
+        }
+
+
+        private void AiRandom()
+        {
+            while (true)
+            {
+                int index = this._rnd.Next(0, 9);
+                if (this._board[index] == "")
+                {
+                    this._board[index] = this._currentPlayer;
+                    CheckWinCondition();
+                    if (!this._gameOver)
+                    {
+                        this._currentPlayer = this._currentPlayer == "X" ? "O" : "X";
+                    }
+                    break;
+                }
+            }
+        }
+        private void AiBestMove(string aiSymbol)
+        {
+            bool done = TryToWinOrBlock(aiSymbol);
+            if (!done)
+            {
+                bool blocked = TryToWinOrBlock(aiSymbol == "X" ? "O" : "X");
+                if (!blocked)
+                {
+                    AiRandom();
+                }
+            }
+        }
+        private bool TryToWinOrBlock(string symbol)
+        {
+            int[][] lines = new int[][]
+            {
+        new int[]{0,1,2}, new int[]{3,4,5}, new int[]{6,7,8},
+        new int[]{0,3,6}, new int[]{1,4,7}, new int[]{2,5,8},
+        new int[]{0,4,8}, new int[]{2,4,6}
+            };
+            for (int i = 0; i < lines.Length; i++)
+            {
+                int a = lines[i][0];
+                int b = lines[i][1];
+                int c = lines[i][2];
+
+                if (_board[a] == symbol && _board[b] == symbol && _board[c] == "")
+                {
+                    _board[c] = _currentPlayer;
+                    CheckWinCondition();
+                    if (!_gameOver)
+                    {
+                        _currentPlayer = _currentPlayer == "X" ? "O" : "X";
+                    }
+                    return true;
+                }
+                if (_board[a] == symbol && _board[b] == "" && _board[c] == symbol)
+                {
+                    _board[b] = _currentPlayer;
+                    CheckWinCondition();
+                    if (!_gameOver)
+                    {
+                        _currentPlayer = _currentPlayer == "X" ? "O" : "X";
+                    }
+                    return true;
+                }
+                if (_board[a] == "" && _board[b] == symbol && _board[c] == symbol)
+                {
+                    _board[a] = _currentPlayer;
+                    CheckWinCondition();
+                    if (!_gameOver)
+                    {
+                        _currentPlayer = _currentPlayer == "X" ? "O" : "X";
+                    }
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void CheckWinCondition()
+        {
+            int[][] lines = new int[][]
+            {
+                new int[]{0,1,2}, new int[]{3,4,5}, new int[]{6,7,8},
+                new int[]{0,3,6}, new int[]{1,4,7}, new int[]{2,5,8},
+                new int[]{0,4,8}, new int[]{2,4,6}
+            };
+            for (int i = 0; i < lines.Length; i++)
+            {
+                int a = lines[i][0];
+                int b = lines[i][1];
+                int c = lines[i][2];
+                if (this._board[a] != "" &&
+                    this._board[a] == this._board[b] &&
+                    this._board[b] == this._board[c])
+                {
+                    this._gameOver = true;
+                    this._winner = this._board[a];
+                    return;
+                }
+            }
+            bool isDraw = true;
+            for (int i = 0; i < 9; i++)
+            {
+                if (this._board[i] == "")
+                {
+                    isDraw = false;
+                    break;
+                }
+            }
+            if (isDraw)
+            {
+                this._gameOver = true;
+                this._winner = "Draw";
+            }
+        }
+
+        public void ResetGame()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                this._board[i] = "";
+            }
+            this._currentPlayer = "X";
+            this._gameOver = false;
+            this._winner = "";
         }
     }
 }
